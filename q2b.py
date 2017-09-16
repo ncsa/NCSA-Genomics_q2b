f9867b52899c478b13d3936d65d1e74aa9ccac15
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-16 01:26:34 -0500

    Abstracted out encode() and other helper functions to Encode.py. q2b.py now will only contain logic for calling Encode.encode()

---
+++ q2b.py
@@ -0,0 +1,11 @@
+""" q2b.py - Ryan Chui (2017) """
+
+import Module.Encode as Encode
+
+def main():
+    """ Either encode or decode a fastq file. """
+
+    Encode.encode()
+
+if __name__ == "__main__":
+    main()
