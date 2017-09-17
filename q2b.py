67774169ec1514456b7d49f68c07144faaa7e1f7
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-17 00:04:35 -0500

    Moved the file reading and added logic to only encode the sequence line

--- q2b.py
+++ q2b.py
@@ -1,11 +1,26 @@
 """ q2b.py - Ryan Chui (2017) """
 
+import sys
 import Modules.Encode as Encode
 
 def main():
-    """ Either encode or decode a fastq file. """
+    """ Either encode or decode a fastq file.
 
-    Encode.encode()
+    Expects:
+        python3+ q2b.py [-e/-d] [/input/path] [/output/path]
+    """
 
+    with open(sys.argv[2], 'r') as i_stream:
+        with open(sys.argv[3], 'a') as o_stream:
+            line_number = 0
+            for line in i_stream:
+                # print(line_number, line)
+                if line_number == 2:
+                    Encode.encode(line.strip(), o_stream)
+                else:
+                    if line[0] == '@':
+                        line_number = 1
+                    o_stream.write(line)
+                line_number += 1
 if __name__ == "__main__":
     main()
